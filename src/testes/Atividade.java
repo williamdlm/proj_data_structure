23870f15838370bd791ba597351de10f62d37a58
William Mota <williammota.david@gmail.com>
2021-06-07 21:08:50 -0300

    Metodo de exclusão e alteração adicionados e funcionais

--- src/testes/Atividade.java
+++ src/testes/Atividade.java
@@ -2,9 +2,16 @@ package testes;
 
 public class Atividade {
 	public static void main(String[] args) {
-		// Testando método de ordenação
-		String[] nomes = adicionarArray(new String[] { "AA", "a", "aab", "AAbei", "Antonio" }, "Abc");
-		System.out.println(nomes.length);
+		// Testando método de inserção
+		// String[] nomes = adicionarArray(new String[] { "AA", "a", "aab", "AAbei",
+		// "Antonio" }, "Abc");
+
+		// Testando método de exclusão
+		// String[] nomes = excluirArray(new String[] { "Will", "Joao", "Marc", "Lari",
+		// "Altair" }, "Marc");
+
+		// Testando método de alteração
+		String[] nomes = alterarArray(new String[] { "Will", "Joao", "Marc", "Lari", "Altair" }, "Will", "William");
 
 		// Imprime os nomes
 		for (int i = 0; i < nomes.length; i++) {
@@ -27,7 +34,7 @@ public class Atividade {
 				// int menor;
 				// int count = 0;
 
-				// A primeira letra do nome2 é maior do que a primeira letra do nome1 ?
+				// A string do nome2 é maior do que a string do nome1 ?
 				if (nome2.toLowerCase().compareTo(nome1.toLowerCase()) > 0) {
 					// utiliza a variável auxiliar e trocar os nomes de posição no array.
 					aux = nome1;
@@ -42,6 +49,7 @@ public class Atividade {
 		return arrayNomes;
 	}
 
+	// Metodo de inclusão
 	public static String[] adicionarArray(String[] arrayNomes, String newName) {
 		String[] newArray = new String[arrayNomes.length + 1];
 		for (int i = 0; i <= arrayNomes.length; i++) {
@@ -54,4 +62,42 @@ public class Atividade {
 		}
 		return newArray;
 	}
+
+	// Metodo de exclusão
+	public static String[] excluirArray(String[] arrayNomes, String nameDeleted) {
+		String[] newArray = new String[arrayNomes.length - 1];
+		String aux = "";
+		for (int i = 0; i <= arrayNomes.length - 2; i++) {
+			if (arrayNomes[i] != nameDeleted) {
+				newArray[i] = arrayNomes[i];
+			} else {
+				aux = newArray[i];
+				newArray[i] = arrayNomes[arrayNomes.length - 1];
+				arrayNomes[arrayNomes.length - 1] = aux;
+
+			}
+			if (i == arrayNomes.length - 2) {
+				newArray = ordenar(newArray);
+			}
+
+		}
+		return newArray;
+	}
+
+	// Metodo de alteração
+	public static String[] alterarArray(String[] arrayNomes, String alteredName, String newName) {
+		String[] newArray = new String[arrayNomes.length];
+		for (int i = 0; i <= arrayNomes.length - 1; i++) {
+			if (arrayNomes[i] == alteredName) {
+				arrayNomes[i] = newName;
+			}
+			newArray[i] = arrayNomes[i];
+			if (i == arrayNomes.length - 1) {
+				newArray = ordenar(newArray);
+			}
+
+		}
+		return newArray;
+	}
+
 }
